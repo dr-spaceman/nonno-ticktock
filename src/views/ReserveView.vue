d3a675411383876f4d9f1fdd06f8ab76d0a1e9b7
Matt Berti <mat.berti@gmail.com>
2022-08-13 16:10:29 -0400

    Initial commit

---
+++ src/views/ReserveView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="reserve">
+    <h1>This is the reserve page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .reserve {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
