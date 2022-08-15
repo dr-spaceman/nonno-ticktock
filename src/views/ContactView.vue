2afea0b28ac263c86978312f1134fc322b604cf1
Matt Berti <mat.berti@gmail.com>
2022-08-14 22:35:16 -0400

    Adjust Contact page style and layout

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,46 @@
+<script setup>
+const contactDetails = {
+  name: 'Gary Berti',
+  phone: `<a href="tel:4102746338">410-274-6338</a>`,
+  email: `<a href="mailto:gary@nonnonticktocks.com">gary@nonnonticktocks.com</a>`,
+}
+</script>
+
 <template>
   <div class="contact">
-    <h1>This is the contact page</h1>
+    <h1>Contact Us</h1>
+    <dl>
+      <template v-for="(value, key, index) in contactDetails" :key="index">
+        <dt>{{ key }}</dt>
+        <dd v-html="value"></dd>
+      </template>
+    </dl>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .contact {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.contact {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+dl {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1em;
+}
+
+dt {
+  font-weight: bold;
+  flex-shrink: 0;
+}
+
+dd {
+  margin: 0;
+  padding: 0;
+  flex: 1 1 80%;
 }
 </style>
