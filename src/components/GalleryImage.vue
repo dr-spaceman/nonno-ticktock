3f835259ad5fa410dacd1dc96d342bcd3cb23da9
Matt Berti <mat.berti@gmail.com>
2022-08-14 22:24:33 -0400

    Add about page with gallery

---
+++ src/components/GalleryImage.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <picture>
+    <source :srcset="`/gallery/${src}--small.webp`" type="image/webp" />
+    <img :src="`/gallery/${src}--small.jpg`" :alt="alt" />
+  </picture>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,64 @@
+<script setup>
+import GalleryImage from '../components/GalleryImage.vue'
+
+const images = [
+  { src: 'house-front', alt: 'Front view of the beach house' },
+  { src: 'porch-sunset', alt: 'The porch at sunset, overlooking the bay dock' },
+  { src: 'nonnos-chair', alt: 'Comfy chair for viewing water traffic' },
+  { src: 'strawberry-room', alt: 'Downstairs bedroom' },
+]
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>About</h2>
+    <p>
+      Nonno & Ticktock's is a house nestled between the sandy treeline and the
+      shores of Rehoboth Bay.
+    </p>
+    <p>
+      With dock access, four bedrooms, and a short drive to the beaches at
+      Rehoboth, it's a delightful retreat for families and groups of friends.
+    </p>
+
+    <ul class="gallery">
+      <li v-for="{ src, alt } in images" :key="src">
+        <GalleryImage :src="src" :alt="alt" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.gallery {
+  max-height: 800px;
+  margin: 2em 0 0;
+  display: flex;
+  gap: 8px;
+  list-style: none;
+  overflow: auto;
+  scroll-snap-type: x mandatory;
+}
+
+.gallery li {
+  scroll-snap-align: start;
+  width: 100%;
+  height: 600px;
+  margin: 0;
+  padding: 0;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+}
+
+.gallery img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
